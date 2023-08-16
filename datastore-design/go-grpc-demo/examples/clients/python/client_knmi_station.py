9873bc6ebc9af21e4181826280dc0b699028de5d
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2023-08-16 15:30:15 +0200

    Param wise or station wise iteration.

--- datastore-design/go-grpc-demo/examples/clients/python/client_knmi_station.py
+++ datastore-design/go-grpc-demo/examples/clients/python/client_knmi_station.py
@@ -21,22 +21,28 @@ if __name__ == '__main__':
     with grpc.insecure_channel(f"{os.getenv('DSHOST', 'localhost')}:{os.getenv('DSPORT', '50050')}") as channel:
         client = dstore_grpc.DatastoreStub(channel=channel)
 
-        # time_series = []
-        observations = []
         # TODO: How to deal with IDs. At the moment, I set them manually, but if the database or server could handle it,
         #   it would help when going for parallel processing when inserting. Do we want to use a UUID?
         ts_id = 1
-        with xr.open_mfdataset(paths=data_paths, combine="by_coords", engine="netcdf4") as file:
-            for param_id in param_ids:
-                param_start = perf_counter()
-                param_slice_start = perf_counter()
-                param_file = file[param_id]
-                print(f"Param slice ({param_id}: {perf_counter() - param_slice_start}")
-                for station_id, latitude, longitude, height in zip(
+        with xr.open_mfdataset(paths=data_paths, combine="by_coords", engine="netcdf4", chunks=-1) as file:
+        # with xr.open_dataset("/home/vervoort/project/e-soh/e-soh-datastore-poc/test-data/KNMI/20221231.nc", engine="netcdf4", chunks=-1) as file:
+            for station_id, latitude, longitude, height in zip(
                     file["station"].values,
                     file["lat"].values[0], file["lon"].values[0],
                     file["height"].values[0]
-                ):
+            ):
+                # time_series = []
+                observations = []
+
+                station_slice_start = perf_counter()
+                station_slice = file.sel(station=station_id)
+                print(f"Station slice ({station_id}): {perf_counter() - station_slice_start}")
+
+                for param_id in param_ids:
+                    param_start = perf_counter()
+                    param_slice_start = perf_counter()
+                    param_file = station_slice[param_id]
+                    print(f"Param slice ({param_id}: {perf_counter() - param_slice_start}")
                     print(f"Starting with time series {ts_id}, {station_id}, {param_id}")
                     tsMData = dstore.TSMetadata(
                         station_id=station_id,
@@ -54,12 +60,9 @@ if __name__ == '__main__':
 
                     client.AddTimeSeries(request)
 
-                    station_slice_start = perf_counter()
-                    sub_set = param_file.sel(station=station_id)
-                    print(f"Station slice ({station_id}, {param_id}): {perf_counter() - station_slice_start}")
                     # TODO check if timestamp is correctly inserted
                     observations_start = perf_counter()
-                    for time, obs_value in zip(sub_set["time"].data.astype("datetime64[s]").astype("int64"), sub_set.data):
+                    for time, obs_value in zip(param_file["time"].data.astype("datetime64[s]").astype("int64"), param_file.data):
                         # single_observation_start = perf_counter()
                         observations.append(
                             dstore.TSObservations(
@@ -83,16 +86,16 @@ if __name__ == '__main__':
                     print(f"Observations for ({station_id}, {param_id}): {perf_counter() - observations_start}")
                     ts_id += 1
 
-                    print("Bulk inserting observations.")
-                    bulk_obs_request_start = perf_counter()
-                    request = dstore.PutObsRequest(
-                        tsobs=observations
-                    )
-                    print(f"PutObsRequest: {perf_counter() - bulk_obs_request_start}.")
-                    bulk_obs_insert_start = perf_counter()
-                    client.PutObservations(
-                        request=request,
-                    )
-                    print(f"Finished bulk insert {perf_counter() - bulk_obs_insert_start}.")
-            print(f"Param ({param_id}: {perf_counter() - param_start}")
+                print(f"Bulk inserting {len(observations)} observations.")
+                bulk_obs_request_start = perf_counter()
+                request = dstore.PutObsRequest(
+                    tsobs=observations
+                )
+                print(f"PutObsRequest: {perf_counter() - bulk_obs_request_start}.")
+                bulk_obs_insert_start = perf_counter()
+                client.PutObservations(
+                    request=request,
+                )
+                print(f"Finished bulk insert {perf_counter() - bulk_obs_insert_start}.")
+                print(f"Param ({station_id}: {perf_counter() - param_start}")
     print(f"Finished, total time elapsed: {perf_counter() - total_time_start}")
