dfa27bec60a955c0749be094a488f88ec7c36e4a
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2023-08-21 11:51:13 +0200

    Refactor, format file.

--- datastore-design/go-grpc-demo/examples/clients/python/client_knmi_station.py
+++ datastore-design/go-grpc-demo/examples/clients/python/client_knmi_station.py
@@ -4,15 +4,19 @@ import os
 from pathlib import Path
 from time import perf_counter
 
-from dummy_data import param_ids
+import grpc
 import xarray as xr
+
 import datastore_pb2 as dstore
 import datastore_pb2_grpc as dstore_grpc
-import grpc
+from dummy_data import param_ids
 
-if __name__ == '__main__':
+# data_paths = Path(Path(__file__).parents[5], "test-data", "KNMI").resolve().glob("*.nc")
+file_path = Path(Path(__file__).parents[5] / "test-data" / "KNMI" / "20221231.nc")
+
+
+if __name__ == "__main__":
     total_time_start = perf_counter()
-    # data_paths = Path(Path(__file__).parents[5], "test-data", "KNMI").resolve().glob("*.nc")
 
     # TODO: The coords are not the same for every timeseries. There are 4 out of the 432 observations that have a
     #   different lat, lon and height. For the test data we use the first row. In the future we should look at
@@ -25,9 +29,10 @@ if __name__ == '__main__':
         #   it would help when going for parallel processing when inserting. Do we want to use a UUID?
         ts_id = 1
         # with xr.open_mfdataset(paths=data_paths, combine="by_coords", engine="netcdf4", chunks=-1) as file:
-        with xr.open_dataset(Path(Path(__file__).parents[5] / "test-data" / "KNMI" / "20221231.nc"), engine="netcdf4", chunks=-1) as file:
+        with xr.open_dataset(file_path, engine="netcdf4", chunks=-1) as file:
+
             for station_id, latitude, longitude, height in zip(
-                    file["station"].values, file["lat"].values[0], file["lon"].values[0], file["height"].values[0]
+                file["station"].values, file["lat"].values[0], file["lon"].values[0], file["height"].values[0]
             ):
                 ts_observations = []
                 station_slice = file.sel(station=station_id)
@@ -41,7 +46,7 @@ if __name__ == '__main__':
                         lon=longitude,
                         other1=param_file.name,
                         other2=param_file.long_name,
-                        other3='value3',
+                        other3="value3",
                     )
                     request = dstore.AddTSRequest(
                         id=ts_id,
@@ -52,26 +57,25 @@ if __name__ == '__main__':
 
                     # TODO check if timestamp is correctly inserted
                     observations = []
-                    for time, obs_value in zip(param_file["time"].data.astype("datetime64[s]").astype("int64"), param_file.data):
-                        observations.append(dstore.Observation(
-                                        time=time,
-                                        value=obs_value,
-                                        metadata=dstore.ObsMetadata(
-                                            field1="KNMI",
-                                            field2="Royal Dutch Meteorological Institute"
-                                        )
-                                    ))
+                    for time, obs_value in zip(
+                        param_file["time"].data.astype("datetime64[s]").astype("int64"), param_file.data
+                    ):
+                        observations.append(
+                            dstore.Observation(
+                                time=time,
+                                value=obs_value,
+                                metadata=dstore.ObsMetadata(
+                                    field1="KNMI", field2="Royal Dutch Meteorological Institute"
+                                ),
+                            )
+                        )
 
                     ts_observations.append(dstore.TSObservations(tsid=ts_id, obs=observations))
                     ts_id += 1
 
-                print(f"Bulk inserting {len(ts_observations)} observations.")
-                request = dstore.PutObsRequest(
-                    tsobs=ts_observations
-                )
+                print(f"Bulk inserting {len(ts_observations)} timeseries, each has {len(observations)} observations.")
+                request = dstore.PutObsRequest(tsobs=ts_observations)
                 bulk_obs_insert_start = perf_counter()
-                client.PutObservations(
-                    request=request,
-                )
+                client.PutObservations(request=request)
                 print(f"Finished bulk insert {perf_counter() - bulk_obs_insert_start}.")
     print(f"Finished, total time elapsed: {perf_counter() - total_time_start}")
