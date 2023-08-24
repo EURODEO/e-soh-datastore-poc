48c3c12f0039a7da3ec527cdefae9ff705a1e303
Jeffrey Vervoort <jeffrey.vervoort@knmi.nl>
2023-08-24 15:58:01 +0200

    Test the change to protobuf timestamp for the KNMI param script.

--- datastore-design/go-grpc-demo/examples/clients/python/client_knmi_param.py
+++ datastore-design/go-grpc-demo/examples/clients/python/client_knmi_param.py
@@ -5,6 +5,9 @@ import os
 from pathlib import Path
 from time import perf_counter
 
+import pandas as pd
+from google.protobuf.timestamp_pb2 import Timestamp
+
 import datastore_pb2 as dstore
 import datastore_pb2_grpc as dstore_grpc
 import grpc
@@ -55,30 +58,39 @@ if __name__ == "__main__":
                         id=ts_id,
                         metadata=tsMData,
                     )
+                    client.AddTimeSeries(request)
 
                     add_ts_request_messages.append(request)
 
                     station_slice = param_file.sel(station=station_id)
                     # TODO check if timestamp is correctly inserted
-                    observations = []
+                    # observations = []
                     for time, obs_value in zip(
-                        station_slice["time"].data.astype("datetime64[s]").astype("int64"), station_slice.data
+                        pd.to_datetime(station_slice["time"].data).to_pydatetime(), station_slice.data
+                        # station_slice["time"].data.astype("datetime64[s]").astype("int64"), station_slice.data
                     ):
-                        # single_observation_start = perf_counter()
+                        observations = []
+                        ts = Timestamp()
+                        ts.FromDatetime(time)
                         observations.append(
                             dstore.Observation(
-                                time=time,
+                                time=ts,
                                 value=obs_value,
                                 metadata=dstore.ObsMetadata(
                                     field1="KNMI", field2="Royal Dutch Meteorological Institute"
                                 ),
                             )
                         )
-                    ts_observations.append(dstore.TSObservations(tsid=ts_id, obs=observations))
+                        obs = [dstore.TSObservations(tsid=ts_id, obs=observations)]
+                        request = dstore.PutObsRequest(tsobs=obs)
+                        print(request)
+                        client.PutObservations(request)
+
+                    # ts_observations.append(dstore.TSObservations(tsid=ts_id, obs=observations))
                     ts_id += 1
 
-                request = dstore.PutObsRequest(tsobs=ts_observations)
-                put_observations_messages.append(request)
+                # request = dstore.PutObsRequest(tsobs=ts_observations)
+                # put_observations_messages.append(request)
 
         print(f"Collected all data in {perf_counter() - collect_time_start} s")
 
@@ -86,16 +98,16 @@ if __name__ == "__main__":
         add_time_start = perf_counter()
         # for request in add_ts_request_messages:
         #     client.AddTimeSeries(request)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-            results = list(executor.map(client.AddTimeSeries, add_ts_request_messages))
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        #     results = list(executor.map(client.AddTimeSeries, add_ts_request_messages))
         print(f"Added all time series in {perf_counter() - add_time_start} s")
 
         print("Insert all the data...")
         insert_time_start = perf_counter()
         # for request in put_observations_messages:
         #     client.PutObservations(request)
-        with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
-            results = list(executor.map(client.PutObservations, put_observations_messages))
+        # with concurrent.futures.ThreadPoolExecutor(max_workers=8) as executor:
+        #     results = list(executor.map(client.PutObservations, put_observations_messages))
         print(f"Inserted all data in {perf_counter() - insert_time_start} s")
 
     print(f"Finished, total time elapsed: {perf_counter() - total_time_start} s")
